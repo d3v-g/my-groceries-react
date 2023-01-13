4fa685763f8b79fb37a3225926f7c775fb34e75e
d3v-g <janewang@wangjiayes-MacBook-Air.local>
2023-01-13 16:04:19 +0000

    style: items and category display

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -11,20 +11,21 @@ export default function Item({
         <div className={`item ${item.highlighted ? 'item--active' : ''}`}>
             <div className='item--title'>
                 <p>{item.name}</p>
-                <button
-                    className='edit--button'
-                    id={item.id}
-                    onClick={event => {handleClick(event, 'edit', 'item')}}>
-                    <img src={editImage} />
-                </button >
-                <button
-                    className='delete--button'
-                    id={item.id}
-                    onClick={event => handleClick(event, 'delete', 'item')} 
-                >
-                    <img src={deleteImage} />
-                </button>
-                
+                <div className='item--buttons'>
+                    <button
+                        className='edit--button'
+                        id={item.id}
+                        onClick={event => {handleClick(event, 'edit', 'item')}}>
+                        <img src={editImage} />
+                    </button >
+                    <button
+                        className='delete--button'
+                        id={item.id}
+                        onClick={event => handleClick(event, 'delete', 'item')} 
+                    >
+                        <img src={deleteImage} />
+                    </button> 
+                </div>
             </div>
             <p className='item--price'>{formatPrice(currency, item.price)}</p>
             <p className='item--note'>note: {item.note ?? ''}</p>
--- src/forms/CurrencyForm.jsx
+++ src/forms/CurrencyForm.jsx
@@ -2,7 +2,7 @@ export default function CurrencyForm({ currency, submitResponse }) {
     return (
         <div>
             <form className='items--currency'>
-                <label htmlFor='currency'>Choose your currency</label>
+                <label htmlFor='currency'>Choose your currency:</label>
                 <select value={currency ?? '$'} onChange={e => submitResponse(e.target.value)}>
                     <option value='$'>$</option>
                     <option value='£'>£</option>
